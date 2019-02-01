11c1dc88ca0d183afd1edf1560ac7437fd7685e0
Nicolas <Nicolas.Pla@corbisstudio.com>
2019-02-01 16:49:19 -0300

    Login update

---
+++ app/src/main/java/com/example/seccion06/LoginActivity.java
@@ -0,0 +1,95 @@
+package com.example.seccion06;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private SharedPreferences prefs;
+
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private Switch switchRemember;
+    private Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        prefs = getSharedPreferences("Preferences", Context.MODE_PRIVATE);
+        bindUI();
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = editTextEmail.getText().toString();
+                String password = editTextPassword.getText().toString();
+
+                if(login(email, password)){
+
+                    goToMain();
+                }
+            }
+        });
+    }
+
+    private void bindUI (){
+
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        switchRemember = findViewById(R.id.switchRemember);
+        btnLogin = findViewById(R.id.buttonLogin);
+    }
+
+    private boolean login(String email, String password){
+        if (!isValidEmail(email)){
+            Toast.makeText(this,"Email is not valid, please try again", Toast.LENGTH_LONG).show();
+            return false;
+        } else if (!isValidPassword(password)) {
+            Toast.makeText(this, "Email is not valid, please try again", Toast.LENGTH_LONG).show();
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    private void saveOnPreferences(String email, String password){
+        if (switchRemember.isChecked()){
+
+            // Objeto editor para poder escribir en los shared preferences
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putString("email", email);
+            editor.putString("pass", password);
+            // editor.commit();  // Frena el codigo hasta guardar los archivos Syncronized
+            editor.apply();   // El codigo sigue mientras en segundo plano se guardan los archivos Asyncronized
+        }
+    }
+
+    private boolean isValidEmail(String email){
+        return !TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+    private boolean isValidPassword(String password){
+
+        return password.length() > 4;
+    }
+
+    private void goToMain(){
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        intent.setFlags(intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
+}
+
+
+
--- app/src/main/java/com/example/seccion06/MainActivity.java
+++ app/src/main/java/com/example/seccion06/MainActivity.java
@@ -1,7 +1,18 @@
 package com.example.seccion06;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +20,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menu_logout:
+                return  true;
+            case R.id.menu_forget_logout:
+                return true;
+                default:
+                     return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void logOut(){
+
+
+    }
+
+    private void removeSharedPreferences(){
+
+
     }
 }
